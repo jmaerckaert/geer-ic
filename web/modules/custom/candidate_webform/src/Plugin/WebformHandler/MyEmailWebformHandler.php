dfc3a9abec5abacb38860509fa49e16bf513dc25
Jonathan Maerckaert <jmaerckaert@gmail.com>
2018-04-14 13:14:58 +0200

    Add new config

---
+++ web/modules/custom/candidate_webform/src/Plugin/WebformHandler/MyEmailWebformHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\my_module\Plugin\WebformHandler;
+
+use Drupal\webform\Plugin\WebformHandler\EmailWebformHandler;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Emails a webform submission.
+ *
+ * @WebformHandler(
+ * id = "local_email",
+ * label = @Translation("Local email"),
+ * category = @Translation("Notification"),
+ * description = @Translation("Sends a webform to candidate email."),
+ * cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ * results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ * )
+ */
+class MyEmailWebformHandler extends EmailWebformHandler {
+
+  /**
+   * Send message to the candidate email.
+   */
+  public function sendMessage(WebformSubmissionInterface $webform_submission, array $message) {
+
+    $candidate_id = \Drupal::request()->query->get('candidate');
+    $candidate = Node::load($candidate_id);
+    $email = $candidate->get('field_candidate_email')->getValue();
+
+    $message['to_mail'] = $email;
+
+    parent::sendMessage($webform_submission, $message);
+  }
+
+}
